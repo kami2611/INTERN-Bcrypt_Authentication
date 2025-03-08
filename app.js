9ab060c7331a3df42be00055d4dea982e3eb1ac9
Kami <166147703+kami2611@users.noreply.github.com>
2025-03-09 00:47:44 +0500

    users can successfully register now

---
+++ app.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+mongoose.connect('mongodb://127.0.0.1:27017/Intern-Authentication').then(() => {
+    console.log("Mongoose Server Started!");
+}).catch((err) => {
+    console.log("Err mongoose!");
+});
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+app.get('/home', (req, res)=>{
+    res.render('home');
+})
+app.post('/register', async(req, res)=>{
+    try {
+        const {username, password} = req.body;
+    console.log(username);
+    console.log(password);
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({username,password: hashedPassword});
+    await newUser.save();
+    return res.render('home', {message: 'successfully registered'});
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.listen(3000, ()=>{
+    console.log('connected on port 3000');
+})
