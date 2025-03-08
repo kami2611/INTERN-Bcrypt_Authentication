567006885658854aa863672ebcd74365cb716919
Kami <166147703+kami2611@users.noreply.github.com>
2025-03-09 01:06:07 +0500

    feat/ added the login functionality

--- app.js
+++ app.js
@@ -3,32 +3,50 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('./models/user');
 const app = express();
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1:27017/Intern-Authentication').then(() => {
     console.log("Mongoose Server Started!");
 }).catch((err) => {
     console.log("Err mongoose!");
 });
-app.get('/register', (req, res)=>{
+app.get('/register', (req, res) => {
     res.render('register');
 });
-app.get('/home', (req, res)=>{
-    res.render('home');
-})
-app.post('/register', async(req, res)=>{
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post('/login', async (req, res) => {
     try {
-        const {username, password} = req.body;
-    console.log(username);
-    console.log(password);
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = new User({username,password: hashedPassword});
-    await newUser.save();
-    return res.render('home', {message: 'successfully registered'});
+        const { username, password } = req.body;
+        console.log('the login username: ', username);
+        console.log('the login password: ', password);
+        const findUser = await User.findOne({ username });
+        if (findUser) {
+            if (bcrypt.compare(password, findUser.password)) {
+                return res.render('home', {message: 'logged you in buddy'});
+            };
+        }
     } catch (error) {
         console.log(error);
     }
 });
-app.listen(3000, ()=>{
+app.get('/home', (req, res) => {
+    res.render('home');
+})
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(username);
+        console.log(password);
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+        return res.render('home', { message: 'successfully registered' });
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.listen(3000, () => {
     console.log('connected on port 3000');
 })
