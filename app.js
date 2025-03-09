36c71fe1cc3c030e2374d4c850d0ce004aeccfdc
Kami <166147703+kami2611@users.noreply.github.com>
2025-03-09 14:18:25 +0500

    added sessions integration with some fixes

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('./models/user');
+const session = require('express-session');
 const app = express();
+app.use(session({
+    secret: '1234',
+    saveUninitialized:false,
+    resave:false,
+}));
+const isLogged = (req, res, next)=>{
+    if(!req.session.isAuth)
+    {
+        console.log(req.path);
+        req.session.returnTo = req.path;
+        return res.redirect('login');
+    }
+    next();
+};
+const isAlreadyLoggedIn = (req, res, next)=>{
+    if(req.session.isAuth)
+    {
+        return res.redirect('/home');
+    }
+    next();
+}
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1:27017/Intern-Authentication').then(() => {
@@ -10,10 +32,33 @@ mongoose.connect('mongodb://127.0.0.1:27017/Intern-Authentication').then(() => {
 }).catch((err) => {
     console.log("Err mongoose!");
 });
-app.get('/register', (req, res) => {
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+app.get('/topSecret', isLogged, (req, res)=>{
+    return res.send('THIS IS TOP SECRET SHOWN ONLY TO LOGGED IN USERS');
+});
+
+app.get('/register',isAlreadyLoggedIn, (req, res) => {
     res.render('register');
 });
-app.get('/login', (req, res) => {
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(username);
+        console.log(password);
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+        req.session.isAuth = true;
+        req.session.username = username;
+        return res.render('home', { message: 'successfully registered' });
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.get('/login',isAlreadyLoggedIn, (req, res) =>{
     res.render('login');
 });
 app.post('/login', async (req, res) => {
@@ -24,25 +69,16 @@ app.post('/login', async (req, res) => {
         const findUser = await User.findOne({ username });
         if (findUser) {
             if (bcrypt.compare(password, findUser.password)) {
-                return res.render('home', {message: 'logged you in buddy'});
+                req.session.isAuth = true;
+                if(req.session.returnTo)
+                {
+                    return res.redirect(`${req.session.returnTo}`);
+                }
+                else{
+                    return res.render('home', {message: 'logged you in buddy'});
+                }
             };
         }
-    } catch (error) {
-        console.log(error);
-    }
-});
-app.get('/home', (req, res) => {
-    res.render('home');
-})
-app.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        console.log(username);
-        console.log(password);
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const newUser = new User({ username, password: hashedPassword });
-        await newUser.save();
-        return res.render('home', { message: 'successfully registered' });
     } catch (error) {
         console.log(error);
     }
